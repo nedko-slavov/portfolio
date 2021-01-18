378859092461c5b2bc87ce4436fbeecf3266014e
Nedko Slavov <n.p.slavov@gmail.com>
2021-01-18 15:33:18 +0200

    Skills refactoring

--- components/Skills.js
+++ components/Skills.js
@@ -1,24 +1,30 @@
 import React from 'react';
 
+const skills = [
+  { id: 'sad', name: 'CSS3' },
+  { id: 'das', name: 'SASS' },
+  { id: 'vff', name: 'HTML5' },
+  { id: 'vdfb', name: 'Javascript' },
+  { id: 'vdf', name: 'React' },
+  { id: 'csd', name: 'Typescript' },
+  { id: 'ntyny', name: 'React Native' },
+  { id: 'sdvbs', name: 'Styled components' },
+  { id: 'dsvvs', name: 'Nextjs' },
+  { id: 'cwecw', name: 'Graphql' },
+  { id: 'cwecwe', name: 'Webpack' },
+  { id: 'cwegwe', name: 'Ruby on Rails (basics)' },
+  { id: 'cwevwe', name: 'Git' },
+  { id: 'vewvwe', name: 'Sketch' },
+];
+
 const Skills = () => (
   <div className="skills-warpper skills-warpper-animation">
     <div className="container">
       <h4 className="section-title skills-title-animation">Skills and Tools</h4>
       <ul className="skills spacing-top-sm">
-        <li className="skill skill-animation">CSS3</li>
-        <li className="skill skill-animation">SASS</li>
-        <li className="skill skill-animation">HTML5</li>
-        <li className="skill skill-animation">Javascript</li>
-        <li className="skill skill-animation">React</li>
-        <li className="skill skill-animation">Typescript</li>
-        <li className="skill skill-animation">React Native</li>
-        <li className="skill skill-animation">Styled components</li>
-        <li className="skill skill-animation">Nextjs</li>
-        <li className="skill skill-animation">Graphql</li>
-        <li className="skill skill-animation">Webpack</li>
-        <li className="skill skill-animation">Ruby on Rails (basics)</li>
-        <li className="skill skill-animation">Git</li>
-        <li className="skill skill-animation">Sketch</li>
+        {skills.map((skill) => (
+          <li key={skill.id} className="skill skill-animation">{skill.name}</li>
+        ))}
       </ul>
     </div>
   </div>
