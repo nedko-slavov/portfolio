c824f7cb9f17543222817bfc47779b488ff24e97
Nedko Slavov <n.p.slavov@gmail.com>
2021-01-18 02:12:33 +0200

    Skills animations

--- components/Skills.js
+++ components/Skills.js
@@ -1,24 +1,24 @@
 import React from 'react';
 
 const Skills = () => (
-  <div className="skills-warpper">
+  <div className="skills-warpper skills-warpper-animation">
     <div className="container">
-      <h4 className="section-title">Skills and Tools</h4>
+      <h4 className="section-title skills-title-animation">Skills and Tools</h4>
       <ul className="skills spacing-top-sm">
-        <li className="skill">CSS3</li>
-        <li className="skill">SASS</li>
-        <li className="skill">HTML5</li>
-        <li className="skill">Javascript</li>
-        <li className="skill">React</li>
-        <li className="skill">Typescript</li>
-        <li className="skill">React Native</li>
-        <li className="skill">Styled components</li>
-        <li className="skill">Nextjs</li>
-        <li className="skill">Graphql</li>
-        <li className="skill">Webpack</li>
-        <li className="skill">Ruby on Rails (basics)</li>
-        <li className="skill">Git</li>
-        <li className="skill">Sketch</li>
+        <li className="skill skill-animation">CSS3</li>
+        <li className="skill skill-animation">SASS</li>
+        <li className="skill skill-animation">HTML5</li>
+        <li className="skill skill-animation">Javascript</li>
+        <li className="skill skill-animation">React</li>
+        <li className="skill skill-animation">Typescript</li>
+        <li className="skill skill-animation">React Native</li>
+        <li className="skill skill-animation">Styled components</li>
+        <li className="skill skill-animation">Nextjs</li>
+        <li className="skill skill-animation">Graphql</li>
+        <li className="skill skill-animation">Webpack</li>
+        <li className="skill skill-animation">Ruby on Rails (basics)</li>
+        <li className="skill skill-animation">Git</li>
+        <li className="skill skill-animation">Sketch</li>
       </ul>
     </div>
   </div>
